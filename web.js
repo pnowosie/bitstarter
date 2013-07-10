54e39c581128c64a5a0b840f5bc6c29da165a8a3
Pawel Nowosielski <nowosielski@gmail.com>
2013-07-10 22:42:36 +0200

    Include index.html and update framework

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
+
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
